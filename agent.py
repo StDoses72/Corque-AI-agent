cc6fa0422f3f207da2e1ff4286b5bb9aa87e3dbf
StDoses72 <stephen.xuxiangcheng@gmail.com>
2025-12-30 00:32:23 +0800

    Update agent.py
    delete the original way of letting AI to output json form. Instead, use the tool call method in ollama to achieve the function calling

--- agent.py
+++ agent.py
@@ -3,51 +3,64 @@ import json
 import requests
 import time
 
-sysPrompt = """
-你是一个本地助理。你可以调用以下工具：
-1. getWeather(location): 获取指定城市天气。
 
-如果用户要求执行这些操作，请只输出以下 JSON 格式，不要说任何废话：
-{"tool": "函数名", "args": {"参数名": "参数值"}}
-如果你判断内容不存在于提供的工具中，你可以直接回答用户的问题，而不需要调用任何工具。
-"""
 class chatter:
-    def __init__(self,systemPrompt=sysPrompt):
+    def __init__(self):
         self.model = 'gpt-oss:20b'
-        self.chatHistory = [{'role': 'system', 'content': systemPrompt}]
-        self.systemPrompt = systemPrompt
+        self.chatHistory = []
 
 def getWeather(location):
-    jsonurl = f"https://wttr.in/{location}?format=j1"#'https://api.open-meteo.com/v1/forecast?latitude=31.2222&longitude=121.4581&current=temperature_2m,relative_humidity_2m'
-    jsonresponse = requests.get(jsonurl)
-    startTime = time.time()
-    url = f"https://wttr.in/{location}?format=3"
-    response = requests.get(url)
-    endTime = time.time()
-    diff = endTime - startTime
-    print(f"请求耗时: {diff} 秒") 
-    return response.text
+    '''
+    Get the temperature of a location
+    Args:
+        location: The name of location
+    Returns:
+        The current temperature for the location 
+    '''
+    def searchWeather(location):
+        try:
+            jsonurl = f"https://wttr.in/{location}?format=j1"#'https://api.open-meteo.com/v1/forecast?latitude=31.2222&longitude=121.4581&current=temperature_2m,relative_humidity_2m'
+            jsonresponse = requests.get(jsonurl)
+            startTime = time.time()
+            url = f"https://wttr.in/{location}?format=3"
+            response = requests.get(url,timeout=10)
+            endTime = time.time()
+            diff = endTime - startTime
+            print(f"请求耗时: {diff} 秒") 
+            return response.text
+        except Exception as e:
+            return f'查询天气时发生错误：{str(e)}'
+    return searchWeather(location)
 
 
 
 def runAgent(chatter):
     userInput = input("User: ")
+    chatter.chatHistory.append({'role': 'user', 'content': userInput})
     response = ollama.chat(
         model = chatter.model,
-        messages=chatter.chatHistory+[{'role': 'user', 'content': userInput}]
+        messages=chatter.chatHistory,
+        tools=[getWeather]
     )
-    chatter.chatHistory.append({'role': 'assistant', 'content': response['message']['content']})
-    chatter.chatHistory.append({'role': 'user', 'content': userInput})
-    content = response['message']['content']
-    print(f"Assistant: {content}")
+    content = response.message.content
+    chatter.chatHistory.append({'role': 'assistant', 'content': content})
+    if response.message.tool_calls:
+        for call in response.message.tool_calls:
+            if call.function.name == 'getWeather':
+                result = getWeather(**call.function.arguments)
+            else:
+                result = 'Unknown Tools'
+            chatter.chatHistory.append({'role': 'tool',  'tool_name': call.function.name, 'content': str(result)})
+            finalResponse = ollama.chat(
+                model = chatter.model,
+                messages=chatter.chatHistory,
+                tools=[getWeather]
+            )
+            print(f'Assistant: {finalResponse.message.content}')
+    else:
+        print(f"Assistant: {content}")
 
-def getJsonToFunc(content):
-    jsonIndexStart = content.find('{')
-    jsonIndexEnd = content.rfind('}') + 1
 
-    jsonInfo = content[jsonIndexStart:jsonIndexEnd]
-    command = json.loads(jsonInfo)
-    return command
 
 def main():
     Corque = chatter()

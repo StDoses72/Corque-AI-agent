267818e4cdd387fa6eae04956837b72f636307ad
StDoses72 <stephen.xuxiangcheng@gmail.com>
2026-02-05 14:47:20 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -9,10 +9,10 @@ load_dotenv(find_dotenv())
 
 class Settings:
     def __init__(self):
-        self.emailUser = os.getenv('GMAIL_EMAIL_USER')
-        self.emailPass = os.getenv('GMAIL_EMAIL_PASS')
-        self.smtpServer = os.getenv('GMAIL_SMTP_SERVER')
-        self.imapServer = os.getenv('GMAIL_IMAP_SERVER')
+        self.emailUser = os.getenv('EMAIL_USER')
+        self.emailPass = os.getenv('EMAIL_PASS')
+        self.smtpServer = os.getenv('SMTP_SERVER')
+        self.imapServer = os.getenv('IMAP_SERVER')
         self.modelName = "gpt-oss:120b-cloud"#'qwen3:8b'
         self.toolModelName = 'qwen3:0.6b'
         self.apiKey = os.getenv('OPENAI_API_KEY')
